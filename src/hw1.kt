08c0662ef02297fbbe73abd28f7b3916e8a2d7e0
Pikin <pikin@bk.ru>
2024-04-01 16:02:26 +0500

    task fixed

--- src/hw1.kt
+++ src/hw1.kt
@@ -2,35 +2,47 @@ fun main() {
 
     while (true) {
         println("Введите команду: ")
-        val str: String? = readlnOrNull()
-        if (str == null) {
+        val inputStr: String? = readlnOrNull()
+
+        if (inputStr == null) {
             continue
         } else {
-            if (str == "exit") {
+            if (inputStr == "exit") {
                 break
             } else {
-                val lst = str.split(" ")
-                if (lst.size != 3) {
+                val lst = inputStr.split(" ")
+                if (lst.size != 4) {
                     println("Ошибка ввода")
                     continue
                 }
                 if (lst[0] == "add") {
-                    if (lst[2].startsWith("+")) {
-                        if (lst[2].substring(1, lst[2].length - 1).matches(Regex("""[0-9]+"""))) {
-                            println("${lst[1]} ${lst[2]}")
-                        } else {
-                            println("Ошибка в номере телефона")
-                            continue
-                        }
-                    } else if (lst[2].matches(Regex("[a-z]+@[a-z]+[.][a-z]+$"))) {
-                        println("${lst[1]} ${lst[2]}")
-                    } else {
-                        println("Ошибка при вводе данных")
-                    }
-                } else {
-                    println("Ошибка ввода")
+                    if (lst[2] == "phone")
+                        addPhone(lst[2], lst[3])
+                    else if (lst[2] == "email")
+                        addEmail(lst[2], lst[3])
+                    else
+                        println("Ошибка ввода")
+
                 }
             }
+        }
+    }
+}
+
+fun addEmail(s: String, s1: String) {
+    if (s1.matches(Regex("[a-z]+@[a-z]+[.][a-z]+$"))) {
+        println("$s $s1")
+    } else {
+        println("Ошибка при вводе данных")
+    }
+}
+
+fun addPhone(str: String, str2: String) {
+    if (str2.startsWith("+")) {
+        if (str2.substring(1, str.length - 1).matches(Regex("""[0-9]+"""))) {
+            println("$str $str2")
+        } else {
+            println("Ошибка в номере телефона")
         }
     }
 }
